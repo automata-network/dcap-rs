1f1cfe8b222d189164c75bcd36f2563cc3dbd525
Preston Ong <preston4896@me.com>
2025-06-25 20:31:45 +0800

    added e2e v5 test

--- src/lib.rs
+++ src/lib.rs
@@ -421,7 +421,7 @@ mod tests {
         let quote = include_bytes!("../data/quote_tdx.bin");
         let quote = Quote::read(&mut quote.as_slice()).unwrap();
 
-        let tcb_info_and_qe_identity_issuer_chain = include_bytes!("../data/signing_cert.pem");
+        let tcb_info_and_qe_identity_issuer_chain = include_bytes!("../data/v5/signing_cert.pem");
         let tcb_info_and_qe_identity_issuer_chain =
             cert_chain_processor::load_pem_chain_bpf_friendly(
                 tcb_info_and_qe_identity_issuer_chain,
@@ -451,6 +451,40 @@ mod tests {
         (collateral, quote)
     }
 
+    fn tdx_quote_v5_data() -> (Collateral, Quote<'static>) {
+        let quote = include_bytes!("../data/v5/alibaba_quote_5.dat");
+        let quote = Quote::read(&mut quote.as_slice()).unwrap();
+
+        let tcb_info_and_qe_identity_issuer_chain = include_bytes!("../data/v5/signing_cert.pem");
+        let tcb_info_and_qe_identity_issuer_chain =
+            cert_chain_processor::load_pem_chain_bpf_friendly(
+                tcb_info_and_qe_identity_issuer_chain,
+            )
+            .unwrap();
+
+        let root_ca_crl = include_bytes!("../data/intel_root_ca_crl.der");
+        let root_ca_crl = CertificateList::from_der(root_ca_crl).unwrap();
+
+        let tcb_info = include_bytes!("../data/v5/tcbinfov3_90C06F000000.json");
+        let tcb_info: TcbInfoAndSignature = serde_json::from_slice(tcb_info).unwrap();
+
+        let qe_identity = include_bytes!("../data/v5/qe_td.json");
+        let qe_identity: QuotingEnclaveIdentityAndSignature =
+            serde_json::from_slice(qe_identity).unwrap();
+
+        let platform_ca_crl = include_bytes!("../data/pck_platform_crl.der");
+        let platform_ca_crl = CertificateList::from_der(platform_ca_crl).unwrap();
+
+        let collateral = Collateral {
+            tcb_info_and_qe_identity_issuer_chain,
+            root_ca_crl,
+            pck_crl: platform_ca_crl,
+            tcb_info,
+            qe_identity,
+        };
+        (collateral, quote)
+    }
+
     fn test_sgx_time() -> SystemTime {
         // Aug 29th 4:20pm, ~24 hours after quote was generated
         SystemTime::UNIX_EPOCH + Duration::from_secs(1724962800)
@@ -459,6 +493,11 @@ mod tests {
     fn test_tdx_time() -> SystemTime {
         // Pinned September 10th, 2024, 6:49am GMT
         SystemTime::UNIX_EPOCH + Duration::from_secs(1725950994)
+    }
+
+    fn test_tdx_v5_time() -> SystemTime {
+        // June 15th, 2025, 12am UTC
+        SystemTime::UNIX_EPOCH + Duration::from_secs(1749945600)
     }
 
     #[test]
@@ -501,5 +540,13 @@ mod tests {
         let (collateral, quote) = tdx_quote_data();
         super::verify_dcap_quote(test_tdx_time(), collateral, quote)
             .expect("certificate chain integrity should succeed");
+    }
+
+    #[test]
+    fn e2e_tdx_v5_quote() {
+        let (collateral, quote) = tdx_quote_v5_data();
+        let output = super::verify_dcap_quote(test_tdx_v5_time(), collateral, quote)
+            .expect("certificate chain integrity should succeed");
+        println!("{:?}", output);
     }
 }
