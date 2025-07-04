270554e1c5d7237196de132a4d1b889374a96d74
Preston Ong <preston4896@me.com>
2025-07-04 16:01:00 +0800

    fixed code commenting

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -22,7 +22,7 @@ const TD15_REPORT_LEN: usize = 648; // TD15_REPORT
 // serialization:
 // [quote_vesion][quote_body_type][tcb_status][fmspc][quote_body_raw_bytes][abi-encoded string array of tcb_advisory_ids]
 // 2 bytes + 2 bytes + 1 byte + 6 bytes + var (SGX_ENCLAVE_REPORT = 384; TD10_REPORT = 584; TD15_REPORT = 648) + var
-// total: 13 + (384 or 584) + var bytes
+// total: 11 + (384 or 584 or 648) + var bytes
 #[derive(Debug)]
 pub struct VerifiedOutput {
     pub quote_version: u16,
